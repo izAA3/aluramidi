8ec75a3057a789776a75ed5249185dba0f988761
izAA3 <129176608+izAA3@users.noreply.github.com>
2023-10-17 09:02:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocasompom(){
+
+document.queryselector('#som_tecla_pom').play('');
+}
+const listadeteclas = document.querySelectorAll('.tecla')
+
+let contador= 0;
+
+ while (contador < 9 ){
+listadeteclas [0]. onclick =tocasompom;
+ }
+ contador = contador + 1;
